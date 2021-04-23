ccbeb8f3d95da249ee551773c4f3497cafb5fa20
Sherry Cuenco <sjcuenco@rocketmail.com>
2021-04-23 14:39:19 -0400

    Angular front end: nav menu, routing, CRUD on department

---
+++ ui/angular11/src/app/department/show-dep/show-dep.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-dep',
+  templateUrl: './show-dep.component.html',
+  styleUrls: ['./show-dep.component.css']
+})
+export class ShowDepComponent implements OnInit {
+
+  constructor(private service: SharedService) { }
+
+  DepartmentList: any = [];
+  ModalTitle: string | undefined;
+  ActivateAddEditDepComp: boolean = false;
+  dep: any;
+
+  DepartmentIdFilter: string = '';
+  DepartmentNameFilter: string = '';
+  DepartmentListWithoutFilter: any = []; //backup of department data.
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+  addClick() {
+    this.dep = {
+      DepartmentId: 0,
+      DepartmentName: ''
+    }
+    this.ModalTitle = "Add Department";
+    this.ActivateAddEditDepComp = true;
+  }
+
+  editClick(item: any) {
+    this.dep = item;
+    this.ModalTitle = "EditDepartment";
+    this.ActivateAddEditDepComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Proceed to delete?')) {
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data => {
+        alert(data.toString());
+        this.refreshDepList();
+      })
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditDepComp = false;
+    this.refreshDepList();
+  }
+
+  refreshDepList() {
+    this.service.getDepList().subscribe( data => {
+      this.DepartmentList = data;
+      this.DepartmentListWithoutFilter = data;
+    });
+  }
+
+  FilterFn() {
+    var DepartmentIdFilter = this.DepartmentIdFilter;
+    var DepartmentNameFilter = this.DepartmentNameFilter;
+
+    this.DepartmentList = this.DepartmentListWithoutFilter.filter(function(el: any) {
+      return el.DepartmentId.toString().toLowerCase().includes(
+        DepartmentIdFilter.toString().toLowerCase()
+      ) &&
+      el.DepartmentName.toString().toLowerCase().includes(
+        DepartmentNameFilter.toString().trim().toLowerCase()
+      )
+    });
+  }
+
+  /* sortResult(prop, asc) {
+    this.DepartmentList = this.DepartmentListWithoutFilter.sort(function(a,b) {
+      if (asc) {
+        return (a[prop]>b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0) ;
+      } else {
+        return (b[prop]>a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0) ;
+      }
+    })
+  } */
+  sortResult(prop: string, asc: boolean) {
+    this.DepartmentList = this.DepartmentListWithoutFilter.sort((a, b) => {
+      if (asc) {
+        return (a[prop]>b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0) ;
+      } else {
+        return (b[prop]>a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0) ;
+      }
+    })
+  }
+}
